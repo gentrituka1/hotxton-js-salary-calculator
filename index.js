e48eae80ec7137cd394bd88241df349c6938b857
Gentrit Uka <gentrit-uka@live.com>
2022-06-13 22:32:26 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+let salary = 500;
+let employeeCont = salary * (5 / 100);
+let taxedSalary = salary - employeeCont;
+let range = 0;
+let taxUnder80 = (80 - 0) * (0/ 100);
+let taxUnder250 = (250-80) * (4 / 100);
+let taxUnder450 = (450-250) * (8 / 100);
+let taxOver450 = (taxedSalary - 450) * (10 / 100);
+
+
+
+
+let totalTax = taxUnder80 + taxUnder250 + taxUnder450 + taxOver450;
+console.log(totalTax);
+let takeHomePay = taxedSalary - totalTax;
+console.log("And the take home pay is: " + takeHomePay);
